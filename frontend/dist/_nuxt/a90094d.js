da3580ddb5f1b738e247f401baadfd27f4c25f92
Mikael Dalholm <mikael@dalholm.se>
2021-12-20 15:45:06 +0100

    Remove builds from develop

--- frontend/dist/_nuxt/a90094d.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{161:function(n,e,t){"use strict";t.r(e);var l=t(32),component=Object(l.a)({},(function(){var n=this,e=n.$createElement;return(n._self._c||e)("h1",[n._v("Hello frontend!")])}),[],!1,null,null,null);e.default=component.exports}}]);
